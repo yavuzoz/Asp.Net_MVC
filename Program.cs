f72d3f74376ba62a88ad531098e529a9e4acbe21
Yavuz Özbay <ozbayavz@gmail.com>
2023-10-30 17:02:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace Asp.Net_MVC
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			// with AddMvc razorpage to use possible
+			builder.Services.AddMvc();
+			var app = builder.Build();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}");
+			app.Run();
+		}
+	}
+}
